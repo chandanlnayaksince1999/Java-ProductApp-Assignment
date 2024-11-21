9f1b6f7905ee1e1c943cb3cb93b0bb1ac444c1e6
chandanlnayak <109851155+chandanlnayaksince1999@users.noreply.github.com>
2024-11-21 13:07:46 +0530

    Delete labs/core-java-labs directory
    Submitting only assignments

--- labs/core-java-labs/src/Java/Self/Study/Constructors/Main.java
+++
@@ -1,9 +0,0 @@
-package Java.Self.Study.Constructors;
-
-public class Main {
-    public static void main(String[] args) {
-        // Creating an object of the Car class
-        Car car1 = new Car("Toyota", 2020);
-        car1.displayInfo();
-    }
-}
